3d962256c29079bed1eccd95d4bf31ac1cca79fe
AwesomlyC <109203183+AwesomlyC@users.noreply.github.com>
2024-02-25 13:58:14 -0800

    added buttons and practice manipulating some DOM objects

--- game.js
+++ game.js
@@ -9,8 +9,43 @@ function getComputerChoice(){
     return choices[(Math.floor(Math.random()*choices.length))];
 }
 
-function playRound(playerChoice, computerChoice){
-    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.substring(1).toLowerCase();
+// function playRound(playerChoice, computerChoice){
+//     playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.substring(1).toLowerCase();
+//     if ((playerChoice == 'Rock' && computerChoice == 'Scissors') ||
+//         (playerChoice == 'Paper' && computerChoice == 'Rock') || 
+//         (playerChoice == 'Scissors' && computerChoice == 'Paper')
+//     ){
+//         console.log(`You Won! ${playerChoice} beats ${computerChoice}`);
+//         playerWin++;
+//     }
+//     else if(playerChoice == computerChoice){
+//         console.log(`It's a tie! Both selected ${playerChoice}`);
+//         ties++;
+//     }
+//     else{
+//         console.log(`You Lose! ${computerChoice} beats ${playerChoice}`);
+//         computerWin++;
+//     }
+// }
+// function playGame(){
+    
+    //     for (let i = 0; i < MAX_GAMES; i++){
+        //         playRound(prompt("Choose your hand: "), getComputerChoice());
+//     }
+//     console.log("Statistics");
+//     console.log(`Player Won: ${playerWin}`);
+//     console.log(`Computer Won: ${computerWin}`)
+//     console.log(`Ties: ${ties}`);
+//     console.log(`Games played: ${MAX_GAMES}`);
+// }
+
+// playGame();
+
+
+function playRound(playerChoice){
+    console.log("Player clicked on: " + playerChoice);
+    const computerChoice = getComputerChoice();
+    console.log("Computer chose: " + computerChoice);
     if ((playerChoice == 'Rock' && computerChoice == 'Scissors') ||
         (playerChoice == 'Paper' && computerChoice == 'Rock') || 
         (playerChoice == 'Scissors' && computerChoice == 'Paper')
@@ -20,25 +55,27 @@ function playRound(playerChoice, computerChoice){
     }
     else if(playerChoice == computerChoice){
         console.log(`It's a tie! Both selected ${playerChoice}`);
-        ties++;
+        playerWin++;
+        computerWin++;
     }
     else{
         console.log(`You Lose! ${computerChoice} beats ${playerChoice}`);
         computerWin++;
     }
-}
-
-function playGame(){
-    
-    for (let i = 0; i < MAX_GAMES; i++){
-        playRound(prompt("Choose your hand: "), getComputerChoice());
+    const container = document.querySelector("#container");
+    const row = document.createElement("div");
+    row.textContent =  playerChoice + " | " + computerChoice + " | " 
+                        + playerWin + "-" + computerWin
+    row.setAttribute('style', 'color: blue; background: white;');
+    container.appendChild(row);
+    if (playerWin + computerWin >= 5){
+        const row = document.createElement("div");
+        let winner;
+        if (playerWin > computerWin)
+            winner = "Player"
+        else
+            winner = "Computer";
+        row.textContent = "Winner is " + winner;
+        container.appendChild(row);
     }
-    console.log("Statistics");
-    console.log(`Player Won: ${playerWin}`);
-    console.log(`Computer Won: ${computerWin}`)
-    console.log(`Ties: ${ties}`);
-    console.log(`Games played: ${MAX_GAMES}`);
 }
-// console.log(getComputerChoice());
-// console.log(playRound("sCiSsOrs", getComputerChoice()));
-playGame();
